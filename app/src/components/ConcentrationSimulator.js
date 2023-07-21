e146b2052a38cf1257421fd52f7b89a43bc2104c
Julian Klug <tensu.wave@gmail.com>
2023-07-21 22:47:21 +0200

    Input forms for patient info

--- app/src/components/ConcentrationSimulator.js
+++ app/src/components/ConcentrationSimulator.js
@@ -1,11 +1,11 @@
 import {makeStyles} from "@material-ui/core/styles";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Graph from "./Graph";
 import getDrugPK from "../pkd_functions/getDrugPK.js";
 import preprocessDose from "../pkd_functions/preprocessDose.js";
 import advanceClosedForm0 from "../pkd_functions/advanceClosedForm0.js";
 import DoseInput from "./DoseInput";
-
+import PatientInfoInput from "./PatientInfoInput";
 
 const useStyles = makeStyles((theme) => ({
     simulator: {
@@ -24,16 +24,38 @@ const useStyles = makeStyles((theme) => ({
 const ConcentrationSimulator = ({}) => {
     const classes = useStyles();
     const [doseHistory, setDoseHistory] = useState([]);
+    const [referenceTime, setReferenceTime] = useState(new Date());
     const [concentrationEvolution, setConcentrationEvolution] = useState([]);
+    const [height, setHeight] = useState(170);
+    const [weight, setWeight] = useState(70);
+    const [age, setAge] = useState(70);
+    const [sex, setSex] = useState('Male');
+    const drug = 'fentanyl';
+    const [drugInfo, setDrugInfo] = useState(getDrugPK(drug, weight, height, age, sex));
 
-    const weight = 70;
-    const height = 170;
-    const age = 50;
-    const sex = 'male'
-    const fentanyl = getDrugPK('fentanyl', weight, height, age, sex);
-    const fentanylPK = fentanyl.PK[0];
+    // maximum time span to compute concentration evolution for (in minutes)
     const maximum = 60;
     const site = 'Effect Site'
+
+    // Use effect to monitor changes in height, weight, age, sex, and update drugInfo
+    useEffect(() => {
+        const newDrugInfo = getDrugPK(drug, weight, height, age, sex)
+        setDrugInfo(newDrugInfo)
+
+        // if doseHistory is not empty, update concentrationEvolution
+        if (doseHistory.length !== 0) {
+            const processedDoseTable = preprocessDose(doseHistory, newDrugInfo.ConcentrationUnits, weight);
+          const drugPK = newDrugInfo.PK[0];
+          const results = advanceClosedForm0(processedDoseTable, drugPK, maximum)
+
+              // set concentration evolution to x (results.time + reference time) and y (results.Cp)
+                const newConcentrationEvolution = results.Time.map((t, i) => {
+                    return {x: new Date(referenceTime + (t * 60 * 1000)), y: results.Ce[i], Cp: results.Cp[i]}
+                });
+            setConcentrationEvolution(newConcentrationEvolution);
+        }
+    }, [height, weight, age, sex]);
+
 
     // Update dose history and concentration evolution when a new dose is added, and return new dose history as well as reference timepoint
     const updateDoseHistory = (newDose) => {
@@ -53,22 +75,28 @@ const ConcentrationSimulator = ({}) => {
   const addDose = (dose, time) => {
         // dose in mcg
       const currentTime = new Date(time);
-      const newDose = { Drug: "fentanyl", TimeDate: currentTime.getTime(), Dose: dose, Units: "mcg" }
+      const newDose = { Drug: drug, TimeDate: currentTime.getTime(), Dose: dose, Units: "mcg" }
 
-      const [newDoseHistory, referenceTime] = updateDoseHistory(newDose);
+      const [newDoseHistory, newReferenceTime] = updateDoseHistory(newDose);
+      setReferenceTime(newReferenceTime);
 
-      const processedDoseTable = preprocessDose(newDoseHistory, fentanyl.ConcentrationUnits, weight);
-      const results = advanceClosedForm0(processedDoseTable, fentanylPK, maximum)
+      const processedDoseTable = preprocessDose(newDoseHistory, drugInfo.ConcentrationUnits, weight);
+      const drugPK = drugInfo.PK[0];
+      const results = advanceClosedForm0(processedDoseTable, drugPK, maximum)
 
       // set concentration evolution to x (results.time + reference time) and y (results.Cp)
         const newConcentrationEvolution = results.Time.map((t, i) => {
-            return {x: new Date(referenceTime + (t * 60 * 1000)), y: results.Ce[i], Cp: results.Cp[i]}
+            return {x: new Date(newReferenceTime + (t * 60 * 1000)), y: results.Ce[i], Cp: results.Cp[i]}
         });
     setConcentrationEvolution(newConcentrationEvolution);
   }
 
     return (
     <div>
+        <div>
+            <PatientInfoInput age={age} height={height} weight={weight}  sex={sex}
+                setAge={setAge} setHeight={setHeight} setWeight={setWeight} setSex={setSex}/>
+        </div>
         <div className={classes.simulator}>
             <Graph data={concentrationEvolution} site={site} />
         </div>
---
+++ app/src/components/PatientInfoInput.js
@@ -0,0 +1,66 @@
+import SensorOccupiedIcon from '@mui/icons-material/SensorOccupied';
+import {makeStyles} from "@material-ui/core/styles";
+import * as React from 'react';
+import {FormControl, InputAdornment, MenuItem, TextField} from "@material-ui/core";
+
+
+
+const useStyles = makeStyles((theme) => ({
+    currentPatientInfo: {
+        alignItems: 'center',
+        display: 'inline-flex',
+        gap: '2vw',
+        marginTop: '2vh',
+        marginLeft: '2vw',
+    },
+}));
+
+export default function PatientInfoInput({height, weight, age, sex, setHeight, setWeight, setAge, setSex}) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.currentPatientInfo}>
+       <SensorOccupiedIcon size="inherit"/>
+            {/*Age:*/}
+            <FormControl sx={{ width: '3ch' }}>
+                <TextField style={{width: "3ch"}} id="standard-basic" label={"Age"} variant="standard" defaultValue={age}
+                onChange={(event) => {
+                          setAge(event.target.value);
+                        }}
+                />
+            </FormControl>
+            {/*Height:*/}
+            <TextField style={{width: "6ch"}} id="standard-basic" variant="standard" label={"Height"} defaultValue={height}
+                               InputProps={{endAdornment: <InputAdornment position="end">cm</InputAdornment>}}
+                    onChange={(event) => {
+                                  setHeight(event.target.value);
+                                }}
+            />
+            {/*Weight: */}
+            <TextField style={{width: "6ch"}} id="standard-basic" variant="standard" label={"Weight"} defaultValue={weight}
+                                 InputProps={{endAdornment:<InputAdornment position="end">kg</InputAdornment>}}
+                    onChange={(event) => {
+                                    setWeight(event.target.value);
+                                    }
+                    }
+            />
+            {/*Sex:  */}
+            <TextField
+                      id="standard-select-sex"
+                      select
+                      defaultValue={sex}
+                      label={"Sex"}
+                        onChange={(event) => {
+                                        setSex(event.target.value);
+                                        }
+                        }
+                    >
+                  {['Male', 'Female'].map((option) => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+        </TextField>
+    </div>
+  );
+}
